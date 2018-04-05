d22d74e2ee2074e662fea99bccfa1b0e608fb78e
pan <panbanglanfeng@gmail.com>
2018-04-04 22:37:45 -0400

    added dnn model

--- intro_to_neural_nets.py
+++ intro_to_neural_nets.py
@@ -37,6 +37,73 @@ def preprocess_targets(california_housing_dataframe):
     return output_targets
 
 
+def construct_feature_columns(input_features):
+    return set([tf.feature_column.numeric_column(my_feature) for my_feature in input_features])
+
+
+def my_input_fn(features, targets, batch_size=1, shuffle=True, num_epochs=None):
+    features = {key: np.array(value) for key, value in dict(features).items()}
+    ds = Dataset.from_tensor_slices((features, targets))
+    ds = ds.batch(batch_size).repeat(num_epochs)
+
+    if shuffle:
+        ds = ds.shuffle(10000)
+
+    features, labels = ds.make_one_shot_iterator().get_next()
+    return features, labels
+
+
+def train_nn_regression_model(learning_rate, steps, batch_size, hidden_units, training_examples, training_targets, validation_examples, validation_targets):
+    periods = 10
+    steps_per_period = steps / periods
+
+    my_optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+    my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
+
+    dnn_regressor = tf.estimator.DNNRegressor(feature_columns=construct_feature_columns(training_examples), hidden_units=hidden_units, optimizer=my_optimizer)
+
+    def training_input_fn(): return my_input_fn(training_examples, training_targets['median_house_value'], batch_size=batch_size)
+
+    def predict_training_input_fn(): return my_input_fn(training_examples, training_targets['median_house_value'], shuffle=False, num_epochs=1)
+
+    def predict_validation_input_fn(): return my_input_fn(validation_examples, validation_targets['median_house_value'], shuffle=False, num_epochs=1)
+
+    print("Training model...")
+    print("RMSE (on training data):")
+    training_rmse = []
+    validation_rmse = []
+
+    for period in range(0, periods):
+        dnn_regressor.train(input_fn=training_input_fn, steps=steps_per_period)
+
+        training_predictions = dnn_regressor.predict(input_fn=predict_training_input_fn)
+        training_predictions = np.array([item['predictions'][0] for item in training_predictions])
+
+        validation_predictions = dnn_regressor.predict(input_fn=predict_validation_input_fn)
+        validation_predictions = np.array([item['predictions'][0] for item in validation_predictions])
+
+        training_root_mean_squared_error = math.sqrt(metrics.mean_squared_error(training_predictions, training_targets))
+        validation_root_mean_squared_error = math.sqrt(metrics.mean_squared_error(validation_predictions, validation_targets))
+
+        print(" period %02d : %0.2f" % (period, training_root_mean_squared_error))
+        training_rmse.append(training_root_mean_squared_error)
+        validation_rmse.append(validation_root_mean_squared_error)
+    print("Model training finished.")
+
+    plt.ylabel("RMSE")
+    plt.xlabel("Periods")
+    plt.title("RMSE vs. Periods")
+    plt.tight_layout()
+    plt.plot(training_rmse, label="training")
+    plt.plot(validation_rmse, label="validation")
+    plt.legend()
+
+    print("Final RMSE (on training data):   %0.2f" % training_root_mean_squared_error)
+    print("Final RMSE (on validation data): %0.2f" % validation_root_mean_squared_error)
+
+    return dnn_regressor
+
+
 california_housing_dataframe = pd.read_csv('data/california_housing_train.csv', sep=",")
 
 california_housing_dataframe = california_housing_dataframe.reindex(np.random.permutation(california_housing_dataframe.index))
@@ -58,3 +125,13 @@ print('Training targets summary:')
 display.display(training_targets.describe())
 print('Validation targets summary:')
 display.display(validation_targets.describe())
+
+dnn_regressor = train_nn_regression_model(
+    learning_rate=0.15,
+    steps=2000,
+    batch_size=800,
+    hidden_units=[10, 10, 8, 6, 4, 2],
+    training_examples=training_examples,
+    training_targets=training_targets,
+    validation_examples=validation_examples,
+    validation_targets=validation_targets)
