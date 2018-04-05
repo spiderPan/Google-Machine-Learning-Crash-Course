72a63f2eb601816bcad491482f7b4e020ae58c65
pan <panbanglanfeng@gmail.com>
2018-04-04 22:09:59 -0400

    added preprocesses functions

--- intro_to_neural_nets.py
+++ intro_to_neural_nets.py
@@ -16,3 +16,26 @@ pd.options.display.float_format = '{:.1f}'.format
 california_housing_dataframe = pd.read_csv('https://storage.googleapis.com/mledu-datasets/california_housing_train.csv', sep=",")
 
 california_housing_dataframe = california_housing_dataframe.reindex(np.random.permutation(california_housing_dataframe))
+
+
+def preprocess_features(california_housing_dataframe):
+    selected_features = california_housing_dataframe[
+        ["latitude",
+         "longitude",
+         "housing_median_age",
+         "total_rooms",
+         "total_bedrooms",
+         "population",
+         "households",
+         "median_income"
+         ]]
+    processed_features = selected_features.copy()
+    processed_features["rooms_per_person"] = processed_features["total_rooms"] / processed_features["population"]
+
+    return processed_features
+
+
+def preprocess_targets(california_housing_dataframe):
+    output_targets = pd.DataFrame()
+    output_targets["median_house_value"] = california_housing_dataframe["median_house_value"] / 1000.0
+    return output_targets
