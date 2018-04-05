79fa823fa998b81e084c9a0f1c92ba8723b5217b
pan <panbanglanfeng@gmail.com>
2018-04-04 22:17:47 -0400

    loading data

--- intro_to_neural_nets.py
+++ intro_to_neural_nets.py
@@ -13,10 +13,6 @@ tf.logging.set_verbosity(tf.logging.ERROR)
 pd.options.display.max_rows = 10
 pd.options.display.float_format = '{:.1f}'.format
 
-california_housing_dataframe = pd.read_csv('https://storage.googleapis.com/mledu-datasets/california_housing_train.csv', sep=",")
-
-california_housing_dataframe = california_housing_dataframe.reindex(np.random.permutation(california_housing_dataframe))
-
 
 def preprocess_features(california_housing_dataframe):
     selected_features = california_housing_dataframe[
@@ -39,3 +35,26 @@ def preprocess_targets(california_housing_dataframe):
     output_targets = pd.DataFrame()
     output_targets["median_house_value"] = california_housing_dataframe["median_house_value"] / 1000.0
     return output_targets
+
+
+california_housing_dataframe = pd.read_csv('data/california_housing_train.csv', sep=",")
+
+california_housing_dataframe = california_housing_dataframe.reindex(np.random.permutation(california_housing_dataframe.index))
+
+training_dataframe = california_housing_dataframe.head(12000)
+training_examples = preprocess_features(training_dataframe)
+training_targets = preprocess_targets(training_dataframe)
+
+validation_dataframe = california_housing_dataframe.tail(50000)
+validation_examples = preprocess_features(validation_dataframe)
+validation_targets = preprocess_targets(validation_dataframe)
+
+print('Training examples summary:')
+display.display(training_examples.describe())
+print('Validation examples summary:')
+display.display(validation_examples.describe())
+
+print('Training targets summary:')
+display.display(training_targets.describe())
+print('Validation targets summary:')
+display.display(validation_targets.describe())
