148cf326e3f94a68acc8e6c8c1903704e80fbf69
pan <panbanglanfeng@gmail.com>
2018-04-12 21:46:24 -0400

    use linear classifier model

--- intro_to_sparse_data_and_embeddings.py
+++ intro_to_sparse_data_and_embeddings.py
@@ -27,7 +27,56 @@ def _parse_function(record):
     return {"terms": terms}, labels
 
 
+def _input_fn(input_filename, num_epoches=None, shuffle=True):
+    ds = tf.data.TFRecordDataset(input_filename)
+    ds = ds.map(_parse_function)
+
+    if shuffle:
+        ds = ds.shuffle(10000)
+    ds = ds.padded_batch(25, ds.output_shapes)
+    ds = ds.repeat(num_epoches)
+
+    features, labels = ds.make_one_shot_iterator().get_next()
+    return features, labels
+
+
 ds = tf.data.TFRecordDataset(train_path)
 ds = ds.map(_parse_function)
 
-ds
+n = ds.make_one_shot_iterator().get_next()
+sess = tf.Session()
+sess.run(n)
+
+informative_terms = ("bad", "great", "best", "worst", "fun", "beautiful",
+                     "excellent", "poor", "boring", "awful", "terrible",
+                     "definitely", "perfect", "liked", "worse", "waste",
+                     "entertaining", "loved", "unfortunately", "amazing",
+                     "enjoyed", "favorite", "horrible", "brilliant", "highly",
+                     "simple", "annoying", "today", "hilarious", "enjoyable",
+                     "dull", "fantastic", "poorly", "fails", "disappointing",
+                     "disappointment", "not", "him", "her", "good", "time",
+                     "?", ".", "!", "movie", "film", "action", "comedy",
+                     "drama", "family")
+terms_feature_column = tf.feature_column.categorical_column_with_vocabulary_list(key="terms", vocabulary_list=informative_terms)
+
+my_optimizer = tf.train.AdagradOptimizer(learning_rate=0.1)
+my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
+
+feature_columns = [terms_feature_column]
+
+classifier = tf.estimator.LinearClassifier(feature_columns=feature_columns, optimizer=my_optimizer)
+classifier.train(input_fn=lambda: _input_fn([train_path]), steps=1000)
+evaluation_metrics = classifier.evaluate(input_fn=lambda: _input_fn([train_path]),
+                                         steps=1000)
+
+print("Training set metrics:")
+for m in evaluation_metrics:
+    print(m, evaluation_metrics[m])
+print('---')
+
+evaluation_metrics = classifier.evaluate(input_fn=lambda: _input_fn([test_path]), steps=1000)
+
+print ('Test set metrics:')
+for m in evaluation_metrics:
+    print(m, evaluation_metrics[m])
+print('---')
